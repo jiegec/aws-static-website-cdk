863984f8e34fb5b54c5987751080e86ddb158502
Jiajie Chen <c@jia.je>
2020-08-17 20:50:41 +0800

    Initial version

--- lib/index.ts
+++ lib/index.ts
@@ -1,32 +1,89 @@
-import * as sns from '@aws-cdk/aws-sns';
-import * as subs from '@aws-cdk/aws-sns-subscriptions';
-import * as sqs from '@aws-cdk/aws-sqs';
 import * as cdk from '@aws-cdk/core';
+import * as s3 from '@aws-cdk/aws-s3';
+import * as s3Deployment from '@aws-cdk/aws-s3-deployment';
+import * as cloudfront from '@aws-cdk/aws-cloudfront';
+import * as path from 'path';
+import * as route53 from '@aws-cdk/aws-route53';
+import * as route53targets from '@aws-cdk/aws-route53-targets';
 
 export interface AwsStaticWebsiteCdkProps {
   /**
-   * The visibility timeout to be configured on the SQS Queue, in seconds.
-   *
-   * @default Duration.seconds(300)
+   * The name of domain zone, e.g. example.com.
    */
-  visibilityTimeout?: cdk.Duration;
+  domainZoneName: string;
+  /**
+   * The record name of website domain, e.g. www.
+   */
+  recordName: string;
+  /**
+   * The source of s3 bucket.
+   */
+  source: s3Deployment.ISource[];
+  /**
+   * The paths to invalidate when source changes.
+   */
+  distributionPaths: string[];
+  /**
+   * The ID of iam certificate
+   */
+  iamCertId: string;
 }
 
 export class AwsStaticWebsiteCdk extends cdk.Construct {
-  /** @returns the ARN of the SQS queue */
   public readonly queueArn: string;
 
-  constructor(scope: cdk.Construct, id: string, props: AwsStaticWebsiteCdkProps = {}) {
+  constructor(scope: cdk.Construct, id: string, props: AwsStaticWebsiteCdkProps) {
     super(scope, id);
 
-    const queue = new sqs.Queue(this, 'AwsStaticWebsiteCdkQueue', {
-      visibilityTimeout: props.visibilityTimeout || cdk.Duration.seconds(300)
+    const hostName = `${props.recordName}.${props.domainZoneName}`;
+
+    const bucket = new s3.Bucket(this, 'WebBucket', {
+      publicReadAccess: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      websiteIndexDocument: "index.html",
+      bucketName: hostName,
+    })
+
+    const distribution = new cloudfront.CloudFrontWebDistribution(this, 'Distribution', {
+      originConfigs: [{
+        customOriginSource: {
+          domainName: bucket.bucketWebsiteDomainName,
+          originProtocolPolicy: cloudfront.OriginProtocolPolicy.HTTP_ONLY
+        },
+        behaviors: [{
+          isDefaultBehavior: true
+        }]
+      }],
+      viewerCertificate: cloudfront.ViewerCertificate.fromIamCertificate(
+        props.iamCertId,
+        {
+          aliases: [hostName]
+        }
+      ),
+      httpVersion: cloudfront.HttpVersion.HTTP2,
+      viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+      priceClass: cloudfront.PriceClass.PRICE_CLASS_ALL
+    })
+
+    const zone = route53.HostedZone.fromLookup(this, 'HostedZone', {
+      domainName: props.domainZoneName
     });
 
-    const topic = new sns.Topic(this, 'AwsStaticWebsiteCdkTopic');
+    new route53.ARecord(this, 'ARecord', {
+      zone,
+      recordName: props.recordName,
+      target: route53.RecordTarget.fromAlias(new route53targets.CloudFrontTarget(distribution)),
+      ttl: cdk.Duration.minutes(5),
+    })
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
-
-    this.queueArn = queue.queueArn;
+    const deployment = new s3Deployment.BucketDeployment(this, "DeployWebsite", {
+      sources: props.source,
+      destinationBucket: bucket,
+      distribution,
+      distributionPaths: props.distributionPaths,
+      retainOnDelete: false,
+      storageClass: s3Deployment.StorageClass.INTELLIGENT_TIERING,
+      cacheControl: [s3Deployment.CacheControl.setPublic(), s3Deployment.CacheControl.maxAge(cdk.Duration.hours(1))]
+    });
   }
 }
--- test/aws-static-website-cdk.test.ts
+++ test/aws-static-website-cdk.test.ts
@@ -1,21 +1,33 @@
 import { expect as expectCDK, haveResource, SynthUtils } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
 import * as AwsStaticWebsiteCdk from '../lib/index';
+import { assert } from 'console';
 
-test('SQS Queue Created', () => {
-    const app = new cdk.App();
-    const stack = new cdk.Stack(app, "TestStack");
-    // WHEN
-    new AwsStaticWebsiteCdk.AwsStaticWebsiteCdk(stack, 'MyTestConstruct');
-    // THEN
-    expectCDK(stack).to(haveResource("AWS::SQS::Queue"));
-});
-
-test('SNS Topic Created', () => {
+test('Static Website Created', () => {
   const app = new cdk.App();
   const stack = new cdk.Stack(app, "TestStack");
+
+  const previous = cdk.ContextProvider.getValue;
+  cdk.ContextProvider.getValue = (_scope: cdk.Construct, options: cdk.GetContextValueOptions) => {
+    assert(options.provider === 'hosted-zone');
+    return {
+      value: {
+        Id: '12345678',
+        Name: 'example.com',
+      },
+    };
+  };
+
+
   // WHEN
-  new AwsStaticWebsiteCdk.AwsStaticWebsiteCdk(stack, 'MyTestConstruct');
+  new AwsStaticWebsiteCdk.AwsStaticWebsiteCdk(stack, 'MyTestConstruct', {
+    domainZoneName: 'example.com',
+    recordName: 'www',
+    source: [],
+    distributionPaths: ['/*'],
+    iamCertId: '1234567890',
+  });
   // THEN
-  expectCDK(stack).to(haveResource("AWS::SNS::Topic"));
+  expectCDK(stack).to(haveResource("AWS::S3::Bucket"));
+  cdk.ContextProvider.getValue = previous;
 });
